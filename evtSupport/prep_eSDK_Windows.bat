0e2dddfff15c61b3546498ab693b6495daabbade
Jakub Wlodek <jwlodek.dev@gmail.com>
2020-08-18 13:54:47 -0700

    Update windows prep script and makefile to build on windows

--- evtSupport/prep_eSDK_Windows.bat
+++ evtSupport/prep_eSDK_Windows.bat
@@ -2,7 +2,8 @@
 SET EVT_DIR=C:\Program Files\EVT
 
 mkdir os
+mkdir os\windows-x64
 
-cp -r "%EVT_DIR%\eSDK\include" .
-cp -r "%EVT_DIR%\eSDK\lib" os\windows-x64
+xcopy /s "%EVT_DIR%\eSDK\include\*.h" .
+xcopy /s "%EVT_DIR%\eSDK\lib\*.*" os\windows-x64
 xcopy /s "%EVT_DIR%\eSDK\bin\*.*" os\windows-x64
