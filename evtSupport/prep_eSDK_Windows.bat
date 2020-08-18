863dbc7a7544f5b4e183be93bada664d6e307a01
Jakub Wlodek <jwlodek.dev@gmail.com>
2020-08-18 12:37:43 -0700

    Minor update

--- evtSupport/prep_eSDK_Windows.bat
+++ evtSupport/prep_eSDK_Windows.bat
@@ -1,8 +1,8 @@
 
-SET EVT_DIR="C:\Program Files\EVT"
+SET EVT_DIR=C:\Program Files\EVT
 
 mkdir os
 
-cp -r %EVT_DIR%\eSDK\include .
-cp -r %EVT_DIR%\eSDK\lib os\windows-x64
-cp %EVT_DIR%\eSDK\bin\* os\windows-x64\.
+cp -r "%EVT_DIR%\eSDK\include" .
+cp -r "%EVT_DIR%\eSDK\lib" os\windows-x64
+xcopy /s "%EVT_DIR%\eSDK\bin\*.*" os\windows-x64
