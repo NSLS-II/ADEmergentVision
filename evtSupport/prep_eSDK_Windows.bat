6d3274e180f84b633e9d1970dccc7cd72bb6bec5
Jakub Wlodek <jwlodek.dev@gmail.com>
2021-01-04 13:41:44 -0500

    Add comments to the prep eSDK script

--- evtSupport/prep_eSDK_Windows.bat
+++ evtSupport/prep_eSDK_Windows.bat
@@ -1,9 +1,14 @@
 
+REM Set location of installed emergent vision SDK
 SET EVT_DIR=C:\Program Files\EVT
 
+REM Create appropriate directories
 mkdir os
 mkdir os\windows-x64
 
+REM Copy include folder and header files
 xcopy /s "%EVT_DIR%\eSDK\include\*.h" .
+
+REM Copy .lib and .dll files to the appropriate locations
 xcopy /s "%EVT_DIR%\eSDK\lib\*.*" os\windows-x64
 xcopy /s "%EVT_DIR%\eSDK\bin\*.*" os\windows-x64
